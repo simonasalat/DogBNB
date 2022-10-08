aff1ffef0a7d5cac6f88713869122df56f13506e
Simona <Simona@ZILVINO-WINDOWS>
2022-10-08 12:40:40 +0300

    Add project files.

---
+++ DogBNB/Program.cs
@@ -0,0 +1,12 @@
+﻿namespace DogBNB
+{
+    public class Program
+    {
+        public static void Main(string[] args)
+        {
+            Console.WriteLine("DOG BNB");
+        }
+
+        
+    }
+}
