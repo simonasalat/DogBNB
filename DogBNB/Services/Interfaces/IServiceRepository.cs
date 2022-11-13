5126c3e39bc98bb83d934059a6d47ef592dea502
gintare <glideikaite@yahoo.com>
2022-11-13 22:49:15 +0200

    added sitter repositories and updated service

---
+++ DogBNB/Services/Interfaces/IServiceRepository.cs
@@ -0,0 +1,17 @@
+﻿using DogBNB.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DogBNB.Services.Interfaces
+{
+    public interface IServiceRepository
+    {
+        public List<Sitter> GetSittersByService();
+        public List<Service> GetSitterServices(int userId);
+        public void AddService(Service service);
+        public void DeleteService(int id);
+    }
+}
